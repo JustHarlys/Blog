baa2afb960ff87127a53ad17b551ecdbe190f69c
JustHarlys <harlysmc@gmail.com>
2024-09-24 02:00:12 -0400

    Agregado el aside, y las entries de manera estatica

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Nav from './Components/Nav'
 import Hero from './Components/Hero'
 import Categories from './Components/Categories'
+import Aside from './Components/Aside'
+import Entries from './Components/Entries'
 import './App.css'
 
 function App() {
@@ -9,7 +11,15 @@ function App() {
     <>
     <Nav />
     <Hero />
-    <Categories />
+    <div className='cat-asd'>
+
+      <div className='cat-ent'>
+
+      <Categories />
+      <Entries />
+      </div>
+    <Aside />
+    </div>
     </>
   )
 }
--- src/Components/Entries.jsx
+++ src/Components/Entries.jsx
@@ -2,9 +2,13 @@
 
 function Entries() {
   return (
-    <>
-    </>
+    <section className="entries-container">
+      <div className="entry">
+        <h1>Titulo de la entrada</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta nulla rhoncus malesuada sodales. Phasellus ornare dolor ante, eget dapibus ex efficitur ac. Praesent condimentum aliquam malesuada. Proin egestas posuere est eleifend porta. Sed at rutrum quam. Nulla vehicula, arcu nec maximus fringilla, purus libero tristique odio, eu pulvinar nulla velit sed metus. Suspendisse purus risus, tincidunt nec pharetra sodales, ullamcorper quis nisl. Maecenas nec pulvinar nisi, quis commodo quam.</p>
+      </div>
+    </section >
   )
 }
 
-export default Entries
+export default Entries;
